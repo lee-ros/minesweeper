bfa2cea32a27b4d95de9a3df42b3526fbe5ec777
Lee Rosenberg <laluk321@gmail.com>
2024-02-19 15:20:25 -0500

    Add a basic cell class

---
+++ minesweeper/cell.py
@@ -0,0 +1,37 @@
+from enum import Enum, auto
+
+
+class CellType(Enum):
+    Uninitialized = auto()
+    Mine = auto()
+    Empty = auto()
+    Number_1 = auto()
+    Number_2 = auto()
+    Number_3 = auto()
+    Number_4 = auto()
+    Number_5 = auto()
+    Number_6 = auto()
+    Number_7 = auto()
+    Number_8 = auto()
+
+
+class CellState(Enum):
+    Opened = auto()
+    Closed = auto()
+    Flagged = auto()
+
+
+class Cell:
+    def __init__(self, row: int, col: int):
+        self._row = row
+        self._col = col
+        self.type = CellType.Uninitialized
+        self.state = CellState.Closed
+
+    @property
+    def row(self) -> int:
+        return self._row
+
+    @property
+    def col(self) -> int:
+        return self._col
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="minesweeper",
+    version="0.0.1",
+    description="A console implementation of the minesweeper game",
+    author="Lee Rosenebrg",
+    author_email="laluk321@gmail.com",
+    url="https://github.com/lee-ros/minesweeper"
+)
